1ac9eba49d7272f0097d4736e5cd03755ad501ce
tdawes93 <tdawes136@googlemail.com>
2021-12-31 13:05:10 +0000

    Add html for results pop up

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,4 @@ const finishQuiz = document.getElementById("submit");
 const playButton = document.getElementById("kick-off");
 const rulesButton = document.getElementById("rules");
 const leagueTableButton = document.getElementById("league-table");
+const quizContainer = document.getElementById("quiz-container");
