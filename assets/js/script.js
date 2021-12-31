53ad49525fde55b74c25ab3a26a2e72593ef5d3f
tdawes93 <tdawes136@googlemail.com>
2021-12-31 12:33:32 +0000

    Add initial variables to script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-console.log("tester");
+const previousQuestion = document.getElementById("previous");
+const nextQuestion = document.getElementById("next");
+const finishQuiz = document.getElementById("submit");
+const playButton = document.getElementById("kick-off");
+const rulesButton = document.getElementById("rules");
+const leagueTableButton = document.getElementById("league-table");
