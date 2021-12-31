b5f384e0ac3cc9b034fd3e29bdd785234c7cd6a7
tdawes93 <tdawes136@googlemail.com>
2021-12-31 15:26:32 +0000

    Add initial button functions and test

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,11 @@ const playButton = document.getElementById("kick-off");
 const rulesButton = document.getElementById("rules");
 const leagueTableButton = document.getElementById("league-table");
 const quizContainer = document.getElementById("quiz-container");
-
+const backBtn = document.getElementsByClassName("back-btn");
+const rulesModal = document.getElementById("rules-container");
+const home = document.getElementById("home");
+const resultsModal = document.getElementById("results-container");
+const leagueTableModal = document.getElementById("league-table-container");
 
 const questions = [
     {question:"...",
@@ -41,17 +45,32 @@ function checkAnswer(){};
 
 function calculateScore(){};
 
-function displayLeagueTable(){};
+function displayLeagueTable(event){
+    leagueTableModal.style.display = "block";
+};
 
 function saveScore(){};
 
-function displayRules(){};
+function displayRules(){
+    rulesModal.style.display = "block";
+};
 
-function returnHome(){};
+function returnHome(){
+    home.style.display = "block";
+    quizContainer.style.display = "none";
+    resultsModal.style.display = "none";
+};
 
 function next(){};
 
 function previous(){};
 
+function back(){
+    rulesModal.style.display = "none";
+    leagueTableModal.style.display = "none";
+};
 
+leagueTableButton.addEventListener = ("onclick", displayLeagueTable);
+
+rulesButton.addEventListener = ("click", displayRules);
 
