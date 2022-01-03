621170bc25fa8ec23df969f41b6a841702ad8b3f
tdawes93 <tdawes136@googlemail.com>
2022-01-03 21:09:03 +0000

    Add answers array const

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,16 +8,17 @@ const rulesModal = document.getElementById("rules-section");
 const leagueTableBtn = document.getElementsByClassName("leaderboard");
 const leagueTableModal = document.getElementById("league-table-section");
 const rematchBtn = document.getElementById("restart");
+const answers = Array.from(document.querySelectorAll('.ans-btn'));
 
 
 
 const questions = [
-    {question:"hello",
+    {question:"How many games did arsenal go unbeaten when they won the invincibles season?",
     answers: {
-        a: "...",
-        b: "....",
-        c: ".....",
-        d: ".."
+        a: "49",
+        b: "56",
+        c: "39",
+        d: "43"
     },
     correctAnswer: "a"},
     {question:"...",
@@ -48,86 +49,26 @@ function buildQuiz() {
     </div>
     <div id="answers-box">
         <label class="answer-option">
-            <input type="radio" name="question${i+1}" value="a" id="a">
+            <input type="radio" name="question${i+1}" value="a" class="ans-btn" id="a">
             ${questions[i].answers.a}
         </label>
         <label class="answer-option">
-            <input type="radio" name="question${i+1}" value="b" id="b">
+            <input type="radio" name="question${i+1}" value="b" class="ans-btn" id="b">
             ${questions[i].answers.b}
         </label>
         <label class="answer-option">
-            <input type="radio" name="question${i+1}" value="c" id="c">
+            <input type="radio" name="question${i+1}" value="c" class="ans-btn" id="c">
             ${questions[i].answers.c}
         </label>
         <label class="answer-option">
-            <input type="radio" name="question${i+1}" value="d" id="d">
+            <input type="radio" name="question${i+1}" value="d" class="ans-btn" id="d">
             ${questions[i].answers.d}
         </label>
     </div>`;
-    
-
-    
-    /*const output = [];
-    questions.forEach(
-        (currentQuestion, questionNumber) => {
-    
-          // variable to store the list of possible answers
-          const answers = [];
-    
-          // and for each available answer...
-          for(letter in currentQuestion.answers){
-    
-            // ...add an HTML radio button
-            answers.push(
-              `<label>
-                <input type="radio" name="question${questionNumber}" value="${letter}">
-                ${letter} :
-                ${currentQuestion.answers[letter]}
-              </label>`
-            );
-          }
-    
-          // add this question and its answers to the output
-          output.push(
-            `<div class="question"> ${currentQuestion.question} </div>
-            <div class="answers"> ${answers.join('')} </div>`
-          );
-        }
-      );
-    
-      // finally combine our output list into one string of HTML and put it on the page
-      quizContainer.innerHTML = output.join('');*/
-    
-    /*for (question of questions) {
-
-        document.getElementById("quiz-container").innerHTML = `<div id="question-count">
-        <h2 id="question-progress">#/#</h2>
-    </div>
-    <div id="questions-box">
-        <p id="question">${question.property[0]}</p>
-    </div>
-    <div id="answers-box">
-        <label class="answer-option">
-            <input type="radio" name="question1" value="a">
-            
-        </label>
-        <label class="answer-option">
-            <input type="radio" name="question1" value="a">
-            answer
-        </label>
-        <label class="answer-option">
-            <input type="radio" name="question1" value="a">
-            answer
-        </label>
-        <label class="answer-option">
-            <input type="radio" name="question1" value="a">
-            answer
-        </label>
-    </div>`
-    }*/
 }
+
 function displayCorrectAnswer(event){
-    let selectedAnswer = document.getElementsByTagName("input").selected;
+    let selectedAnswer = document.getElementsByTagName("input").checked;
     if (selectedAnswer === questions[i].correctAnswer) {
         selectedAnswer.style.backgroundColor = "green";
     } else {
@@ -190,6 +131,6 @@ for (let i = 0; i < backBtn.length; i++) {
     backBtn[i].addEventListener("click", back);
 }
 
-document.getElementsByTagName("input").selected.addEventListener("select", displayCorrectAnswer);
+document.getElementsByTagName("input").selected.addEventListener("click", displayCorrectAnswer);
 
 
